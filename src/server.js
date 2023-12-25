48810e35d3caffc33d411a7ffec353589dca0bd2
bui van song <songbvhe172267@fpt.edu.vn>
2023-12-26 03:53:26 +0700

    TestCase1

---
+++ src/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import bodyParser from "body-parser";
+import viewEngine from "./config/viewEngine";
+import initWebRoutes from "./route/web";
+require('dotenv').config();
+
+let app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+//config app
+viewEngine(app);
+initWebRoutes(app);
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log("backend nodejs is running on the port: "+port);
+});
