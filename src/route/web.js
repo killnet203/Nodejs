ee9ff3ef4d8b6fd9a0db06692db05b3c3d3fe25c
bui van song <songbvhe172267@fpt.edu.vn>
2023-12-26 05:23:27 +0700

    TestCase2

--- src/route/web.js
+++ src/route/web.js
@@ -1,12 +1,14 @@
 import express from "express";
+import homeController from "../controller/homeController";
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
 
-    router.get("/", (req, res) => {
-        return res.send("Hello World with killerWeb")
-    });
+    router.get("/", homeController.getHomePage);
+
+    router.get("/about", homeController.getAboutPage);
+
     return app.use("/", router);
 }
 
